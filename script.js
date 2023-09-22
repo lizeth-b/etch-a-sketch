414ef1917d9bcc315dde985ec127d85ea293ae3b
Liz Baldelomar <hello@lizethb.com>
2023-09-22 17:09:56 +0800

    Add hover effect

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 container.style.width = '960px';
 container.style.height = '960px';
 container.style.display = 'flex';
@@ -8,6 +8,10 @@ for (let i = 0; i < 256; i++) {
   let tile = document.createElement('div');
   tile.style.width = '60px';
   tile.style.height = '60px';
-  tile.style.backgroundColor = 'yellow';
+  tile.style.backgroundColor = '#fef39a';
   container.appendChild(tile);
 }
+
+container.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = '#89c1c3';
+});
