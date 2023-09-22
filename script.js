4c8b77f965d897697efbc7aecf69c8216210bd63
Liz Baldelomar <hello@lizethb.com>
2023-09-22 22:01:45 +0800

    Add prompt to change grid

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+const PAD_SIZE = 960;
+
+function drawPad(side) {
+  container.replaceChildren();
+  for (let i = 0; i < side*side; i++) {
+    let tile = document.createElement('div');
+    tile.style.width = `${PAD_SIZE / side}px`;
+    tile.style.height = `${PAD_SIZE / side}px`;
+    tile.style.backgroundColor = '#fef39a';
+    container.appendChild(tile);
+  }
+}
+
 const container = document.querySelector('.container');
-container.style.width = '960px';
-container.style.height = '960px';
+container.style.width = `${PAD_SIZE}px`;
+container.style.height = `${PAD_SIZE}px`;
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-
-for (let i = 0; i < 256; i++) {
-  let tile = document.createElement('div');
-  tile.style.width = '60px';
-  tile.style.height = '60px';
-  tile.style.backgroundColor = '#fef39a';
-  container.appendChild(tile);
-}
 
 container.addEventListener('mouseover', (e) => {
   e.target.style.backgroundColor = '#89c1c3';
 });
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  let side = Number(prompt('Number of squares per side (max 100):', 100));
+  if (side >= 0 && side <= 100) {
+    drawPad(side);
+  }
+});
+
+drawPad(100);
